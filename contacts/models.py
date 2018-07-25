f9320b93e55a4e8e61a5407acf2acceedddde589
Jean-Mark Wright <jeanmark.wright@gmail.com>
2018-07-24 23:08:10 -0400

    Added Phonebook model and str functions

--- contacts/models.py
+++ contacts/models.py
@@ -1,38 +1,65 @@
 from django.db import models
+from django.conf import settings
 
-from django_extensions.db.models import TimestampedModel
+from django_extensions.db.models import TimeStampedModel
 
 
 from .constants import ContactType
 
 
-class Contact(TimestampedModel):
+class PhoneBook(TimeStampedModel):
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Contact(TimeStampedModel):
 
     first_name = models.CharField(max_length=255, blank=True, null=True)
     last_name = models.CharField(max_length=255, blank=True, null=True)
-    emails = models.ManyToManyField("Email", blank=True)
+    emails = models.ManyToManyField("ContactEmail", blank=True)
     contact_numbers = models.ManyToManyField("ContactNumber", blank=True)
     favourite = models.BooleanField(default=False)
+    phonebook = models.ForeignKey(PhoneBook, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "{} {}".format(self.first_name, self.last_name)
 
 
-class Email(TimestampedModel):
+class ContactEmail(TimeStampedModel):
 
     email = models.CharField(max_length=255, blank=True, null=True)
 
+    def __str__(self):
+        return self.email
 
-class ContactNumber(TimestampedModel):
+
+class ContactNumber(TimeStampedModel):
 
     number = models.CharField(max_length=11, blank=True, null=True)
     contact_type = models.CharField(max_length=10, choices=ContactType.CHOICES)
 
+    def __str__(self):
+        return self.number
 
-class ContactGroup(TimestampedModel):
+
+class ContactGroup(TimeStampedModel):
 
     name = models.CharField(max_length=255)
-    contacts = models.ManyToManyField(blank=True)
+    contacts = models.ManyToManyField(Contact, blank=True)
+
+    def __str__(self):
+        return self.name
 
 
-class CallLog(TimestampedModel):
+class CallLog(TimeStampedModel):
 
-    contact = models.ForeignKey("Contact")
+    contact = models.ForeignKey("Contact", on_delete=models.CASCADE)
     duration = models.IntegerField(default=0)
+
+    def __str__(self):
+        return "Call Log for {}".format(self.contact)
